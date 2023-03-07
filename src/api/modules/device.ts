f6a8944b95bf09f18065606aea5a6dc2547db467
zhangyili <zhangyili@supcon.com>
2023-03-07 18:12:38 +0800

    develop

--- src/api/modules/device.ts
+++
@@ -1,5 +0,0 @@
-import http from "../index";
-import { ReqPage } from "../interface";
-export const getDevicePage = (data: ReqPage) => {
-  return http.get("/supaiot/api/config/device/page", data);
-};
--- src/settings.ts
+++ src/settings.ts
@@ -1,35 +1,5 @@
-export type screenSetting = {
-  webName: string;
-  title: string;
-  isWideScreen: boolean;
-  isAccordingToResolution: boolean;
-};
+export type screenSetting = {};
 
-const settings: screenSetting = {
-  /**
-   * @type String
-   *
-   * 网页名称
-   */
-  webName: "大屏",
-  /**
-   * @type String
-   *
-   * 大屏名称
-   **/
-  title: "大屏",
-  /**
-   * @type Boolean
-   *
-   * 是否是宽屏 width 2560 3840 ...
-   */
-  isWideScreen: true,
-  /**
-   * @type Boolean
-   *
-   * 是否根据分辨率显示数据块
-   */
-  isAccordingToResolution: true,
-};
+const settings: screenSetting = {};
 
 export default settings;
